8e2be3cbd6eb9a6bce3649ecc733ec3dc24ca1be
shimaaa_ty <56390651+mash531@users.noreply.github.com>
2026-05-20 19:07:03 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $data = [
+            'name' => 'onion',
+            'price' => 200,
+            'weight' => 200
+        ];
+       print_r($data);
+        ?>
+    </p>
+</body>
+
+</html>
